9b2b88ee02d4b0f3db59ead6dd3bbe484bb6f6ef
Priyanshu Varshney <priyanshuvarshney027@gmail.com>
2026-06-23 08:37:08 +0530

    priyanshu[add]: oops

--- oops-practice/gcr-codebase/oops-programming-fundamentals/Scenario-based/BankAccount.java
+++ oops-practice/gcr-codebase/oops-programming-fundamentals/Scenario-based/BankAccount.java
@@ -1,42 +1,72 @@
 public class BankAccount{
-    String accountNumber;
-    String accountHolder;
+    int accountNum;
+    String holder;
     double balance;
-    int totalAccount=0;
+    static int totalAccount=0;
 
-    public BankAccount(String accountNumber, String accountHolder, double balance){
-        this.accountHolder = accountHolder;
-        this.accountNumber = accountNumber;
-        this.balance = balance;
+    public BankAccount(int accountNum,String holder,double balance){
+        this.accountNum=accountNum;
+        this.holder=holder;
+        this.balance=balance;
         totalAccount++;
     }
-    public void deposit(String accountNumber, String accountHolder, int amount){
-        if((this.accountNumber.equals(accountNumber)) && (this.accountHolder.equals(accountHolder))){
-            this.balance += amount;
-            System.out.print(amount+" Amount deposited in: "+accountHolder+ " current balance is: "+this.balance);
-        }else{
-            System.out.println("Invalid AccountHolder & Invalid credentials");
-        }
+
+    public void deposit(double amount){
+        balance +=amount;
+        System.out.println(amount+"deposited");
     }
-    public void withdraw(String accountNumber, String accountHolder,int amount){
-        if((this.accountNumber.equals(accountNumber)) && (this.accountHolder.equals(accountHolder))){
-            this.balance -= amount;
-            System.out.print(amount+" Amount withdraw from: "+accountHolder+ " current balance is: "+this.balance);
-        }
-        else{
-            System.err.println("Invalid user");
-        }
+
+    public void withdraw(double amount){
+      if(amount<=balance){
+        balance-=amount;
+        System.out.println(amount + " withdrawn.");
+      }  else{
+       System.out.println("insufficient balance");
+      }
     }
-    public void getStatement(String accountNumber, String accountHolder, double balance){
-        System.out.println("Account number is: "+accountNumber+", Accoun Holder is: "+accountHolder+", Balance is: "+balance);
+    
+    public void getStatement(){
+        System.out.println("accountNum"+ accountNum);
+        System.out.println("holder name"+ holder);
+        System.out.println("balance"+ balance);
+
     }
-    public static void totalUser(int totalAccount){
-        System.err.println("Total Account is: "+totalAccount);
-    }
+
+
+    // public class Main {
+
     public static void main(String[] args) {
-        BankAccount b1 = new BankAccount("1123546880", "None Singh", 5000);
-        BankAccount b2 = new BankAccount("45454545", "jojo", 6300);
-        BankAccount b3 = new BankAccount("69696999", "lolo", 5200);
-        b1.withdraw("1123546880", "None Singh", 2000);
+
+        BankAccount a1 = new BankAccount(101, "Harshita", 10000);
+        BankAccount a2 = new BankAccount(102, "Rahul", 15000);
+        BankAccount a3 = new BankAccount(103, "Priya", 20000);
+
+        a1.deposit(1000);
+        a1.withdraw(500);
+        a1.deposit(2000);
+        a1.withdraw(3000);
+        a1.withdraw(1000);
+ 
+        a2.deposit(5000);
+        a2.withdraw(2000);
+        a2.deposit(1000);
+        a2.withdraw(4000);
+        a2.withdraw(1000);
+        
+        a3.deposit(3000);
+        a3.withdraw(1000);
+        a3.deposit(500);
+        a3.withdraw(7000);
+        a3.withdraw(500);
+
+        System.out.println("ACCOUNT STATEMENTS");
+
+        a1.getStatement();
+        a2.getStatement();
+        a3.getStatement();
+
+        System.out.println("\nTotal Accounts Created : "
+                           + BankAccount.totalAccount);
     }
+// }
 }
